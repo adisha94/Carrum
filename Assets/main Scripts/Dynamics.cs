4a8c738eaf50f287763dbeeebac68114894b11a1
ADISH BETAWAR <abetawar@gmail.com>
2015-10-11 22:50:43 -0700

    removed unnecessary display method
    removed an unncessary method

--- Assets/main Scripts/Dynamics.cs
+++ Assets/main Scripts/Dynamics.cs
@@ -72,8 +72,6 @@ public class Dynamics : MonoBehaviour
 		};
 	}
 
-	void display()
-	{
 
 	}
 
---
+++ Assets/main Scripts/Impact.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+///  This script will help for dealing with object behavior when hit by another object.
+/// 
+/// </summary>
+public class Impact : MonoBehaviour
+{
+
+	private GameObject source; // attached object
+	private GameObject target; // targeted object
+	public float force;
+	// Use this for initialization
+	void Start ()
+	{
+		force = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+
+	void OnColliderEnter(GameObject other)
+	{
+//		Vector3 move = new Vector3 (x, 0, z); move forward
+		// behavior if the object hits another object
+	}
+}
