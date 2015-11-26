c64d09d84156c2cc933518d9fb6ed9024d3b28a6
ADISH BETAWAR <abetawar@gmail.com>
2015-11-26 15:45:26 -0800

    new stuff

---
+++ Assets/TextChange.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextChange : MonoBehaviour 
+{
+	// declaring variables
+	public GUIText sampleText;
+	public GUIText currentText;
+	public GUIText targetText;
+	private int score;
+	// Use this for initialization
+	void Start ()
+	{
+		sampleText.text = "";
+		currentText.text = "Idle Position";
+		targetText.text = "";
+	}
+
+	void OnColliderEnter(Collider other)
+	{
+		currentText.text = "Collision occured";
+	}
+
+}
