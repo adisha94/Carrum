8fdd64211e0d3d3826587d610e6132d28c61ebaa
ADISH BETAWAR <abetawar@gmail.com>
2015-11-26 16:53:30 -0800

    physics testing successful
    successful in testing the spheres. now working on moving the spheres
    based on standard keyboard functionalities

---
+++ Assets/Destroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour
+{
+	void OnCollisionEnter( Collision col )
+	{
+		if (col.gameObject.name == "Sphere 1")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
