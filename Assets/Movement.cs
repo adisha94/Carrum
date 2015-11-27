de2021ed8244aed14429541355c6ad858b113873
ADISH BETAWAR <abetawar@gmail.com>
2015-11-26 17:25:22 -0800

    movement script beta

---
+++ Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+	public float range;
+	// Update is called once per frame
+	void Update ()
+	{
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		float xPos = (h * range) * Time.deltaTime;
+		float yPos = (v * range) * Time.deltaTime;
+		transform.position = new Vector3 (xPos, 0, yPos); // updates the position of the current object
+	}
+
+}
