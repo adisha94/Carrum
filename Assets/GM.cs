edd17f1b610e9927de30cd12457ba06da3f181ed
Adish Betawar <abetawar@gmail.com>
2016-03-12 11:09:20 -0800

    project resolution resolved

---
+++ Assets/GM.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GM : MonoBehaviour 
+{
+	public int lives;
+	public Player_turn;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
